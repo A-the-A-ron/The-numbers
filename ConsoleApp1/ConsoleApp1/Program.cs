2c9269882434749b6b860d37d6f42b3545e7f1b3
Aaron Hansen <30009474@student.toiohomai.ac.nz>
2018-08-03 14:03:54 +1200

    Mark II

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -14,27 +14,35 @@ namespace ConsoleApp1
             do
             {
                 string guess = "";
+                int t = 0;
+                int over= 10;
+                int adder = 1;
                 Console.WriteLine("I'm thinking of a number between 1-500. You have ten guesses. you can say 'I GIVE UP!' at any time to quit.");
                 Random rnd = new Random();
                 int number = rnd.Next(1, 501);
 
-                do
-                {
+
+                do {
+
                     guess = Console.ReadLine();
                     if (guess == "I GIVE UP!")
                     {
                         Console.WriteLine("Very well");
                         break;
                     }
-                    else if (guess != "I GIVE UP!" ){
+                    else if (guess != "I GIVE UP!") {
                         int i = Convert.ToInt32(guess);
                         if (i > number)
                         {
-                            Console.WriteLine("smaller");
+                            Console.WriteLine("smaller.");
+                            t +=adder;
+                            if (t == over) { break; }
                         }
                         else if (i < number)
                         {
                             Console.WriteLine("BIGGER");
+                            t+=adder;
+                            if (t == over) { break; }
                         }
                         else if (i == number)
                         {
@@ -42,18 +50,19 @@ namespace ConsoleApp1
                             guess = "correct";
                             break;
                         }
-                    
 
-                } while (guess != "correct" || guess != "I GIVE UP!");
 
+                    }
+                } while (guess != "correct" || guess != "I GIVE UP!"|| t==over);
+                if (t == over) { Console.WriteLine("You lose!"); }
                 Console.WriteLine("Would you like to play again? (y/n)");
-                leave = Console.ReadLine();
-                if (leave == "n")
-                {
-                    break;
-                }
-            } while (leave != "n");
-            Console.WriteLine("Goodbye");
-        }
+                    leave = Console.ReadLine();
+                    if (leave == "n")
+                    {
+                        break;
+                    }
+                Console.Clear();
+            }while (leave != "n");
+                Console.WriteLine("Goodbye");
     }
-}
+    } }
